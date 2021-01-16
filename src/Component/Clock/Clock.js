0e71e691057c5de1916917c91d3e64961dfe9797
Vranjan7077 <ranvinay123@gmail.com>
2021-01-16 18:23:08 +0530

    First deploy

---
+++ src/Component/Clock/Clock.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import './Clock.css';
+
+class Clock extends Component 
+{
+
+    constructor(props)
+    {
+        super(props);
+        this.state =
+        {
+            time: new Date().toLocaleTimeString()
+        }
+    }
+
+
+    componentDidMount() 
+    {
+            this.intervalID = setInterval(() => this.updateClock(), 1000);
+    }
+
+
+    componentWillUnmount()
+        {
+            clearInterval(this.intervalID)
+        }
+
+        /* to update the clock */
+    updateClock()
+    {
+        this.setState({
+            time: new Date().toLocaleTimeString()
+        });
+    }
+
+
+    render() {
+        return (
+                <div className="Time">
+                    <p> {this.state.time}</p>
+                </div>
+            );}
+    }
+
+
+export default Clock;
